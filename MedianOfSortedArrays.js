93a50decd97988278ba7296a679277a4d15113e5
evrenkaraarslan <evrenkaraarslan@hotmail.com>
2020-05-22 16:40:27 +0200

    LeetCode JavaScript

---
+++ MedianOfSortedArrays.js
@@ -0,0 +1,32 @@
+var findMedianSortedArrays=(nums1, nums2)=> {
+var my_list=[];
+nums1.map(x=> my_list.push(x) );
+nums2.map(y=> my_list.push(y));
+my_list.sort((a, b) => a - b);
+var c= (my_list.length%2===1)? my_list[(my_list.length-1)/2]:(my_list[my_list.length/2]+my_list[(my_list.length/2)-1])/2;
+return c;}
+
+/*
+
+There are two sorted arrays nums1 and nums2 of size m and n 
+respectively.
+
+Find the median of the two sorted arrays. The overall run time 
+complexity should be O(log (m+n)).
+
+You may assume nums1 and nums2 cannot be both empty.
+
+Example 1:
+
+nums1 = [1, 3]
+nums2 = [2]
+
+The median is 2.0
+Example 2:
+
+nums1 = [1, 2]
+nums2 = [3, 4]
+
+The median is (2 + 3)/2 = 2.5
+
+*/
